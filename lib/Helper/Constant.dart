57dfa2937ebf36e45adab1c65e13e43f705118b7
Alphawizz Flutter Team <alphawizzflutterteam@gmail.com>
2024-05-13 13:14:59 +0530

    Initial commit by anjali

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,23 @@
+final String appName = ' Annadata Seller';
+
+final String packageName = 'com.annadata.seller';
+
+final String iosPackage = 'com.annadata.seller';
+
+// final String baseUrl = 'https://vendor.eshopweb.store/seller/app/v1/api/';
+
+// final String jwtKey = "68f05dec6014f68e760c5c5fa3e31bcf391a2e10";
+
+// final String baseUrl = 'https://alphawizztest.tk/annadata1/seller/app/v1/api/';
+final String baseUrl = 'https://developmentalphawizz.com/new_annadata_sukhi_bhav/seller/app/v1/api/';
+
+final String jwtKey = "78084f1698c9fcff5a668b68dcd103db39be2605";
+
+//timeout limit
+final int timeOut = 50;
+const int perPage = 10;
+
+const String BANK_DETAIL = 'Bank Details:\n' +
+    'Account No :123XXXXX\n' +
+    'IFSC Code: 123XXX \n' +
+    'Name: Abc xyz';
